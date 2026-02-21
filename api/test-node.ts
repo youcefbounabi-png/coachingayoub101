75a26a9cc1cae78499387e5c4446e2c79cec2872
youcefbounabi-png <youcefbounabi@gmail.com>
2026-02-21 02:07:40 +0100

    debug: add node environment test

---
+++ api/test-node.ts
@@ -0,0 +1,13 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+    return res.status(200).json({
+        nodeVersion: process.version,
+        fetchAvailable: typeof fetch !== 'undefined',
+        env: {
+            HAS_CHARGILY: !!process.env.CHARGILY_API_KEY,
+            HAS_PAYPAL_ID: !!process.env.PAYPAL_CLIENT_ID,
+            HAS_SUPABASE: !!process.env.VITE_SUPABASE_URL
+        }
+    });
+}
